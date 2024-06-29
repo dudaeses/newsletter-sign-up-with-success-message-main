979b346a79c646d467028b86fe07d218afff284a
dudaeses <me157282@gmail.com>
2024-06-29 17:48:37 -0300

    organizando código e adicionando comentários

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const form = document.getElementById('form');
 const formImg = document.getElementById('form-img');
 const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
-const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; // padrão regex para validação de e-mail
 const successMessage = document.getElementById('success-message');
 const dismissMessage = document.getElementById('dismiss-message');
 const message = document.getElementById('message');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event) => { // verifica o resultado da validação do e-mail quando há um submit no formulário e mostra a mensagem de erro ou de sucesso 
     event.preventDefault();
     if(emailValidate()) {
         const email = document.getElementById('email').value;
@@ -20,22 +20,22 @@ form.addEventListener('submit', (event) => {
     }
 });
 
-dismissMessage.addEventListener('click', () => {
+dismissMessage.addEventListener('click', () => { // fecha a mensagem de sucesso quando há um clique no botão dismiss-message e volta para o formulário de inscrição
     successMessage.style.display = 'none'; 
     form.style.display = "block"; 
 });
 
-function setError(index){
+function setError(index){ // mostra a mensagem de erro
     campos[index].style.border = '2px solid hsl(4, 100%, 67%)';
     spans[index].style.display = 'block'; 
 }
 
-function removeError(index){
+function removeError(index){ // remove a mensagem de erro
     campos[index].style.border = '';
     spans[index].style.display = 'none'; 
 }
 
-function emailValidate(){
+function emailValidate(){ // faz a validação do e-mail
     const email = document.getElementById('email').value; 
     if(!emailRegex.test(email)) {
         setError(0);
