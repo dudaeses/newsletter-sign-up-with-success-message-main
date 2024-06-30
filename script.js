9fe8865850f55c7fcbd6c25415cb98065ee81353
dudaeses <me157282@gmail.com>
2024-06-30 14:35:30 -0300

    finalização da versão desktop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form');
 const formImg = document.getElementById('form-img');
-const campos = document.querySelectorAll('.required');
-const spans = document.querySelectorAll('.span-required');
+const campo = document.getElementsByClassName('required');
+const span = document.getElementsByClassName('span-required');
 const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i; // padrão regex para validação de e-mail
 const successMessage = document.getElementById('success-message');
 const dismissMessage = document.getElementById('dismiss-message');
@@ -23,17 +23,21 @@ form.addEventListener('submit', (event) => { // verifica o resultado da validaç
 dismissMessage.addEventListener('click', () => { // fecha a mensagem de sucesso quando há um clique no botão dismiss-message e volta para o formulário de inscrição
     successMessage.style.display = 'none'; 
     form.style.display = "block"; 
+    formImg.style.display = "block"
 });
 
 function setError(index){ // mostra a mensagem de erro
-    campos[index].style.border = '1px solid hsl(4, 100%, 67%)';
-    campos[index].style.transition = 'border 0.2s ease';
-    spans[index].style.display = 'block'; 
+    campo[index].style.border = '1px solid hsl(4, 100%, 67%)';
+    campo[index].style.background = 'hsl(4, 100%, 92%)';
+    campo[index].style.color = 'hsl(4, 100%, 67%)';
+    campo[index].style.transition = 'border 0.2s ease';
+    span[index].style.display = 'block'; 
 }
 
 function removeError(index){ // remove a mensagem de erro
-    campos[index].style.border = '';
-    spans[index].style.display = 'none'; 
+    campo[index].style.border = '';
+    campo[index].style.color = '';
+    span[index].style.display = 'none'; 
 }
 
 function emailValidate(){ // faz a validação do e-mail
